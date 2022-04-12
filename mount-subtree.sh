783dacee2844d01557d96ab557c98f608c11e988
mrizaln <mrizaln2000@gmail.com>
2022-04-12 07:24:17 +0700

    update mount script

--- mount-subtree.sh
+++ mount-subtree.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/env bash
 
 case $1 in
     "")
-    mkdir -p config-files etc_X11_xorg.conf.d usr_share_X11_xorg.conf.d
+    mkdir -p config-files additional-config-files/etc_X11_xorg.conf.d additional-config-files/usr_share_X11_xorg.conf.d
 
     sudo mount --bind ~/.config config-files/
-    sudo mount --bind /etc/X11/xorg.conf.d/ etc_X11_xorg.conf.d/
-    sudo mount --bind /usr/share/X11/xorg.conf.d/ usr_share_X11_xorg.conf.d/
+    sudo mount --bind /etc/X11/xorg.conf.d/ additional-config-files/etc_X11_xorg.conf.d/
+    sudo mount --bind /usr/share/X11/xorg.conf.d/ additional-config-files/usr_share_X11_xorg.conf.d/
     ;;
     "unmount")
     sudo umount ~/.config config-files/
-    sudo umount /etc/X11/xorg.conf.d/ etc_X11_xorg.conf.d/
-    sudo umount /usr/share/X11/xorg.conf.d/ usr_share_X11_xorg.conf.d/
+    sudo umount /etc/X11/xorg.conf.d/ additional-config-files/etc_X11_xorg.conf.d/
+    sudo umount /usr/share/X11/xorg.conf.d/ additional-config-files/usr_share_X11_xorg.conf.d/
     ;;
     *)
     echo argument not understood
