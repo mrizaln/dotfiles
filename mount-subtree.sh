e1fcacea59dd9bb876f8ce01f267f686340044f1
mrizaln <mrizaln2000@gmail.com>
2022-05-22 20:12:05 +0700

    update mount script

--- mount-subtree.sh
+++ mount-subtree.sh
@@ -4,11 +4,13 @@ case $1 in
     "")
         mkdir -p config-files additional-config-files/etc_X11_xorg.conf.d additional-config-files/usr_share_X11_xorg.conf.d
 
-        sudo mount --bind ~/.config config-files/
+        sudo mount --bind ~/.local/bin/ scripts/
+        sudo mount --bind ~/.config/ config-files/
         sudo mount --bind /etc/X11/xorg.conf.d/ additional-config-files/etc_X11_xorg.conf.d/
         sudo mount --bind /usr/share/X11/xorg.conf.d/ additional-config-files/usr_share_X11_xorg.conf.d/
         ;;
     "unmount")
+        sudo umount scripts/
         sudo umount config-files/
         sudo umount additional-config-files/etc_X11_xorg.conf.d/
         sudo umount additional-config-files/usr_share_X11_xorg.conf.d/
