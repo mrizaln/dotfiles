eb4e3a481a4834026d6d7d82f25299eb750f2bc7
mrizaln <mrizaln2000@gmail.com>
2022-08-19 09:36:49 +0700

    add unzip option and add -r flag to zip command

--- scripts/zip_files.sh
+++ scripts/zip_files.sh
@@ -1,9 +1,16 @@
 #!/bin/env bash
 
+if [[ "$1" == "--unzip" ]]; then
+        notify-send "unzipping $2"
+        unzip "$2"
+        notify-send "unzip completed"
+        exit 0
+fi
+
 filename=$(yad --entry --entry-label="Filename:")
 
 if [[ "$filename" == "" ]]; then exit 0; fi
 
 filename="${filename//.zip/}.zip"
 
-zip "$filename" "$@"
+zip -r "$filename" "$@"
