4c9b8b8c12fbc7fc3f4789e63f85ba6b1b16d565
mrizaln <mrizaln2000@gmail.com>
2022-10-10 23:56:56 +0700

    Add interface variable

--- config-files/polybar/polybar-scripts/vnstat-today.sh
+++ config-files/polybar/polybar-scripts/vnstat-today.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 color="#EBCB8B"     #yellow
+interface=wlp2s0
 
-usage=$(vnstat | grep today | cut -d/ -f3 | tr -s ' ' | cut -d\  -f2-)
+usage=$(vnstat -i $interface | grep today | cut -d\| -f3 | tr -s ' ' | cut -d\  -f2-)
 echo "[%{F$color}${usage%% }%{F-}]"
