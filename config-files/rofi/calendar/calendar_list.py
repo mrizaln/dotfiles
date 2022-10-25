a198dadd953655ac98316aebe9bec154b29ebb6d
mrizaln <mrizaln2000@gmail.com>
2022-10-25 23:10:08 +0700

    Add new parser function and new program argument
    Add RRULE parser (it's not parsed by ics.py library)

--- config-files/rofi/calendar/calendar_list.py
+++ config-files/rofi/calendar/calendar_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
 import arrow
 from ics import Calendar, Event
@@ -13,7 +14,10 @@ def openCalendar(sourceFile: str) -> Calendar:
         calendar = Calendar(file_handler.read())
     return calendar
 
-def getEvents(calendar: Calendar, startTime: float, endTime: float) -> list[Event]:
+def getEvents(calendar: Calendar, startTime: float=-1, endTime: float=-1) -> list[Event]:
+    if startTime == -1 or endTime == -1:
+        return list(calendar.events)
+
     eventList: list[Event] = list()
     for event in iter(calendar.timeline):
         event_begin_time: float = event.begin.timestamp()
@@ -35,7 +39,7 @@ def export(calendar: Calendar, fileName: str) -> None:
     with open(fileName, 'w') as f:
         f.writelines(calendar.serialize_iter())
 
-def printCalendar(sourceFile: str, today: bool=False, timezone: str=TIMEZONE) -> None:
+def printCalendar(sourceFile: str, today: bool=False, timezone: str=TIMEZONE, full: bool=False) -> None:
     calendar = Calendar()
     with open(sourceFile, 'r') as file_handler:
         calendar = Calendar(file_handler.read())
@@ -43,32 +47,65 @@ def printCalendar(sourceFile: str, today: bool=False, timezone: str=TIMEZONE) ->
     if today:
         it = calendar.timeline.today()
         for event in it:
-            begin: str = event.begin.to(timezone).format('HH:mm')
-            end: str = event.end.to(timezone).format('HH:mm')
-            time = begin + " - " + end
-            if event.duration.total_seconds() > 24*60*60:
-                time = f"{event.begin.to(timezone).format('DD/MM')} - {event.end.to(timezone).format('DD/MM')}"
-            print(f"{time:<17}", end='')
-            print(event.name, end="    ")
-            print(f"({event.location})")
+            if full:
+                print(event.begin.to(timezone).format(), end =' - ')
+                print(event.end.to(timezone).format(), end = ' \t ')
+                print(event.name, end = '\t')
+                for ext in event.extra:
+                    print(ext, end=' ')
+                    print()
+            else:
+                begin: str = event.begin.to(timezone).format('HH:mm')
+                end: str = event.end.to(timezone).format('HH:mm')
+                time = begin + " - " + end
+                if event.duration.total_seconds() > 24*60*60:
+                    time = f"{event.begin.to(timezone).format('DD/MM')} - {event.end.to(timezone).format('DD/MM')}"
+                print(f"{time:<17}", end='')
+                print(event.name, end="    ")
+                print(f"({event.location})")
     else:
         it = iter(calendar.timeline)
         for event in it:
-            begin: str = event.begin.to(timezone).format('ddd (HH:mm)')
-            time = begin
-            if event.duration.total_seconds() > 24*60*60:
-                time = f"{event.begin.to(timezone).format('DD/MM')} - {event.end.to(timezone).format('DD/MM')}"
-            print(time, end='\t')
-            print(event.name, end="   ")
-            print(f"({event.location})")
+            if full:
+                print(event.begin.to(timezone).format(), end =' - ')
+                print(event.end.to(timezone).format(), end = ' \t ')
+                print(event.name, end = '\t')
+                for ext in event.extra:
+                    print(ext, end=' ')
+                print()
+            else:
+                begin: str = event.begin.to(timezone).format('ddd (HH:mm)')
+                time = begin
+                if event.duration.total_seconds() > 24*60*60:
+                    time = f"{event.begin.to(timezone).format('DD/MM')} - {event.end.to(timezone).format('DD/MM')}"
+                print(time, end='\t')
+                print(event.name, end="   ")
+                print(f"({event.location})")
 
 
-def cacheCalendar(sourceFile: str, destinationFile: str, startTime: float, endTime: float, timezone: str=TIMEZONE) -> None:
+def cacheCalendar(sourceFile: str, destinationFile: str, startTime: float=-1, endTime: float=-1, timezone: str=TIMEZONE) -> None:
     sourceCalendar = openCalendar(sourceFile)
+    sourceCalendar = parseRRULE(sourceCalendar)
     events = getEvents(sourceCalendar, startTime, endTime)
     newCalendar = createNewCalendar(events, timezone)
     export(newCalendar, destinationFile)
 
+# parse event with RRULE from calendar
+def parseRRULE(calendar: Calendar):
+    calendar_temp: Calendar = calendar.clone()
+    for event in calendar.events:
+        for ext in event.extra:
+            val = str(ext)
+            if val.startswith("RRULE"):
+                rrule = val.split(';')
+                if rrule[2].startswith("COUNT"):
+                    count = int(rrule[2].split("=")[1])        # COUNT=N
+                    for i in range(count):
+                        event_copy = event.clone()
+                        event_copy.end = event_copy.end.shift(weeks=i+1)
+                        event_copy.begin = event_copy.begin.shift(weeks=i+1)
+                        calendar_temp.events.add(event_copy)
+    return calendar_temp
 
 def main():
     parser = argparse.ArgumentParser()
@@ -77,12 +114,10 @@ def main():
                         action="store",
                         metavar="FILE",
                         default=None)
-    parser.add_argument("-c", "--cache", help="cache calendar for N days (using -d option), or from START to END (using -s and -e option)",
-                        action="store_true")
-    parser.add_argument("-p", "--print", help="print a pretty list of all events",
-                        action="store_true")
-    parser.add_argument("--today", help="use with print. print today events (not all events in a file)",
-                        action="store_true")
+    parser.add_argument("-c", "--cache", help="create a cache of calendar (from FILE) for N days (using -d option), or from START to END (using -s and -e option). the CACHE_LOCATION needs to be an absolute path",
+                        action="store",
+                        metavar="CACHE_LOCATION",
+                        default=None)
     parser.add_argument("-s", "--start", help="unix time",
                         action="store",
                         metavar="START",
@@ -91,42 +126,73 @@ def main():
                         action="store",
                         metavar="END",
                         default=None)
-    parser.add_argument("-d", "--days", help="days interval from now",
+    parser.add_argument("-d", "--days", help="days interval from now. set to -1 to get all events",
+                        type=int,
                         action="store",
                         metavar="DAYS",
                         default=None)
+    parser.add_argument("-p", "--print", help="print a pretty list of all events",
+                        action="store_true")
+    parser.add_argument("-pf", "--print-full", help="print full format",
+                        action="store_true")
+    parser.add_argument("--today", help="use with print. print today events (not all events in a file)",
+                        action="store_true")
     parser.add_argument("-tz", "--timezone",
                         action="store",
                         metavar="TIMEZONE",
                         default=None)
+    parser.add_argument("-q", "--quiet", help="don't print anything (except from -p or -pf)",
+                        action="store_true")
 
     args = parser.parse_args()
-    
+
+    QUIET = args.quiet
     CACHE = args.cache
     PRINT = args.print
+    PRINT_FULL = args.print_full
     TZ = args.timezone if args.timezone is not None else TIMEZONE
 
+    if args.file and not QUIET:
+        print(f"using {args.file} as source")
 
     if CACHE:
+        if not QUIET: print(f"using {CACHE} as cache location")
+
+        if CACHE[0] != '/' and CACHE[0] != '~':         # -(c == / or c == ~)    c != / and c != ~
+            print("CACHE_LOCATION needs to be an absolute path")
+            exit(1)
+
         if not args.days and not (args.start and args.end):
             print("if -c option is used, you need to specify -s and -e, or -d")
             exit(1)
 
+        if  not QUIET: print("caching...")
+
         if args.days is not None:
             time_now: float = arrow.utcnow().timestamp()
-            time_later: float = arrow.utcnow().shift(days=int(args.days)).timestamp()
+            time_later: float = arrow.utcnow().shift(days=int(args.days)).timestamp() if args.days != -1 else -1
             source = args.file if args.file is not None else SOURCE_FILE
-            dest = "calendar_cache.ics"
+            dest = os.path.join(CACHE, "calendar_cache.ics")
             cacheCalendar(source, dest, time_now, time_later, TZ)
 
     if PRINT:
         if not args.file:
             print("file needs to be specified")
             exit(1)
-        
+
         file = args.file
         printCalendar(file, args.today, TZ)
 
+    if PRINT_FULL:
+        if not args.file:
+            print("file needs to be specified")
+            exit(1)
+
+        file = args.file
+        printCalendar(file, full=True)
+
+def empty(*args, **kwargs):
+    pass
 
 if __name__ == "__main__":
     main()
--- config-files/rofi/calendar/get_events.sh
+++ config-files/rofi/calendar/get_events.sh
@@ -8,20 +8,20 @@ CALENDAR_FILE="${PARENT_DIR}/calendar.ics"
 CACHED_CALENDAR_FILE="${PARENT_DIR}/calendar_cache.ics"
 
 script="${PARENT_DIR}/calendar_list.py"
-rofi_command="rofi -theme $PARENT_DIR/timetable.rasi -dmenu  -markup-rows -i -p Today"
+rofi_command="rofi -theme $PARENT_DIR/timetable.rasi -dmenu -i"
 
-if ! [[ -e "$CACHED_CALENDAR_FILE" ]]; then
-    $script -c -d 30 -f $CALENDAR_FILE      # create cache of 30 days
-fi
+#if ! [[ -e "$CACHED_CALENDAR_FILE" ]]; then
+#    $script -c $PARENT_DIR -d 30 -f $CALENDAR_FILE      # create cache of 30 days
+#fi
 
 if [[ $option == "--cache" ]]; then
-    $script -c -d $interval -f $CALENDAR_FILE
+    $script -c $PARENT_DIR -d $interval -f $CALENDAR_FILE
 
 elif [[ $option == "--print" ]]; then
-    event=$($script -p -f $CACHED_CALENDAR_FILE | $rofi_command)
+    event=$($script -p -f $CACHED_CALENDAR_FILE -q | $rofi_command -p "This week")
 
 elif [[ $option == "--today" ]]; then
-    event=$($script -p -f $CACHED_CALENDAR_FILE --today | $rofi_command)
+    event=$($script -p -f $CACHED_CALENDAR_FILE -q --today | $rofi_command -p "Today")
 fi
 
 if [[ $event == "" ]]; then exit 0; fi
