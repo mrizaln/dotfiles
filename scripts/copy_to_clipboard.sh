7f3ae4c66e83de61374c9a0d458d67ae44821b07
mrizaln <mrizaln2000@gmail.com>
2022-12-20 05:52:32 +0700

    Refactor and add option to read from stdin

--- scripts/copy_to_clipboard.sh
+++ scripts/copy_to_clipboard.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 
-if [[ "$1" == "" ]]; then
-    echo "usage: $0 <file> [content]"
-    exit 0
+if [[ -p /dev/stdin ]]; then
+    stdin_data=$(cat)        # read stdin
 fi
 
 target=$(file -b --mime-type "$1")
 file="$1"
 content="$2"
 
-if [[ ! ("$content" == "") && (("$target" == "text/plain") || ("$target" == "text/x-shellscript")) ]]; then
-    cat "$1" | xclip -r -sel clip
+# copy content of a text file
+copy_content()
+{
+    file="$1"
+    target_type="$2"
+    cat "$file" | xclip -r -sel clip
 
-    echo "$target"
+    echo "$target_type"
     echo "$file"
     notify-send "Copied contents of  '$file'  to clipboard"
-
-    exit 0
-fi
+}
 
 ### workaround for jpeg
-if [[ ("$target" == "image/jpeg") || ("$target" == "image/jpg") ]]; then
+copy_jpeg()
+{
+    file="$1"
+    target_type="$2"
+
     temp_file="/tmp/$(date +%F_%H-%M-%S).png"
     convert "$file" "$temp_file"        # convert jpeg file into png file in /tmp
     target="image/png"
 
+    xclip -sel clip -target "$target_type" < "$file"
+
+    echo "$target_type"
+    echo "$file"
+    notify-send "Copied  '$file'  to clipboard"
+
+    rm "$temp_file"                                  # remove the converted jpeg file
+}
+
+
+if [[ "$1" == "" ]]; then
+    echo "usage: $0 <file> [content]"
+elif [[ "$1" == "-" ]]; then
+    echo "$stdin_data" | xclip -sel clip -target "text/plain"
+elif [[ ! ("$content" == "") && (("$target" == "text/plain") || ("$target" =~ "text/x-")) ]]; then
+    copy_content "$file" "$target"
+elif [[ ("$target" == "image/jpeg") || ("$target" == "image/jpg") ]]; then
+    copy_jepg "$file" "$target"
+else
+    # do normally
     xclip -sel clip -target "$target" < "$file"
 
     echo "$target"
     echo "$file"
     notify-send "Copied  '$file'  to clipboard"
-
-    rm "$temp_file"                                  # remove the converted jpeg file
-    exit 0
 fi
-### end of workaround
 
-xclip -sel clip -target "$target" < "$file"
-
-echo "$target"
-echo "$file"
-notify-send "Copied  '$file'  to clipboard"
-
-exit 0
