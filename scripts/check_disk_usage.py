c236ec345719a722045e9bad46bf5c2797738430
mrizaln <mrizaln2000@gmail.com>
2022-12-24 04:08:45 +0700

    Fix available space different from df; also modify layout

--- scripts/check_disk_usage.py
+++ scripts/check_disk_usage.py
@@ -4,17 +4,17 @@ import math
 import shutil
 import subprocess
 
+
 class Partition:
-    PATH_MAX_LEN = 30
+    PATH_MAX_LEN = 25
     NAME_MAX_LEN = 10
 
     # size and usedSize is in bytes
     def __init__(self, name: str = "", path: str = "", size: int = 0, usedSize: int = 0) -> None:
-        self.name: str = name if len(name) < self.NAME_MAX_LEN else name[:self.NAME_MAX_LEN-3] + "..."
-        self.path: str = path if len(path) < self.PATH_MAX_LEN else path[:self.PATH_MAX_LEN-3] + "..."
+        self.name: str = name if len(name) < self.NAME_MAX_LEN else name[:self.NAME_MAX_LEN-1] + "…"
+        self.path: str = path if len(path) < self.PATH_MAX_LEN else path[:self.PATH_MAX_LEN-1] + "…"
         self.size: int = size
-        self.usedSize: int = usedSize
-
+        self.usedSize: int = usedSize + 0.05*self.size      # see: https://askubuntu.com/questions/249387/df-h-used-space-avail-free-space-is-less-than-the-total-size-of-home
 
     # size is in kbytes
     @classmethod
@@ -22,9 +22,8 @@ class Partition:
         usedSize: int = int(usedPercentage * size / 100)
         cls.__init__(name, path, size, usedSize)
 
-
     # progress is normalized
-    def print(self, terminalWidth : int):
+    def print(self, terminalWidth: int):
         try:
             progress = self.usedSize / self.size
         except:
@@ -33,7 +32,11 @@ class Partition:
         width = terminalWidth - self.PATH_MAX_LEN - self.NAME_MAX_LEN - 20
 
         free = self.size - self.usedSize
-        info = f"{formatBytes(free):>6} free"
+        free = f"{formatBytes(free):>6} free"
+
+        size = f"{formatBytes(self.size):>6}"
+
+        usedPercentage = f"{round(progress*100, 1):>4}%"
 
         # 0 <= progress <= 1
         progress = min(1, max(0, progress))
@@ -43,10 +46,10 @@ class Partition:
         part_char = [" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉"][part_width]
 
         if (width - whole_width - 1) < 0:
-          part_char = ""
-        bar = "[" + "█" * whole_width + part_char + " " * (width - whole_width - 7) + "]" if terminalWidth >= 100 else ''
+            part_char = ""
+        bar = "[" + "█" * whole_width + part_char + " " * (width - whole_width - 13) + "]" if terminalWidth >= 100 else ''
 
-        line = f"{self.name:<{self.NAME_MAX_LEN}} {self.path:<{self.PATH_MAX_LEN}} {info}  {bar} {round(progress*100, 1):<4} %"
+        line = f"{self.name:<{self.NAME_MAX_LEN}} {self.path:<{self.PATH_MAX_LEN}} {size} {bar} ({usedPercentage}) {free}"
         print(line)
 
 
@@ -93,10 +96,15 @@ def main():
     terminalWidth, _ = shutil.get_terminal_size((80, 20))
 
     # 0 5 1 4
-    for n,line in enumerate(lines):
+    parts = []
+    for n, line in enumerate(lines):
         line = line.split()
         part = Partition(line[0], line[5], int(line[1]), int(line[2]))
 #        print(line[0], line [5], line[1], line[2], sep = '\t')
+        parts.append(part)
+
+    parts = sorted(parts, key=lambda part: 0.95 - part.usedSize/part.size)
+    for part in parts:
         part.print(terminalWidth)
 
 
