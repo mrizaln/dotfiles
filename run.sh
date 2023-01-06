495c1e91b794e33256502fe46e2c8e888c46a413
mrizaln <mrizaln2000@gmail.com>
2023-01-07 04:03:20 +0700

    Update installation(?) script

--- run.sh
+++ run.sh
@@ -1,73 +1,120 @@
 #!/bin/env bash
 
-# install rofi deb package
-sudo dpkg -i 'others/rofi_1.7.0-1_amd64.deb'
-
-# update routine
-sudo apt update; sudo apt upgrade; sudo apt autoremove;
-
-# install necessary packages
-sudo apt install python3 unzip bspwm polybar rofi picom sddm kitty thunar flameshot neofetch sxhkd dunst git lxpolkit lxappearance xorg htop sct brightnessctl alsa-utils pulseaudio pavucontrol variety libglib2.0-0 libglib2.0-bin feh mpv net-tools vnstat papirus-icon-theme fonts-noto-color-emoji fonts-noto-cjk fonts-firacode fonts-font-awesome fonts-ubuntu fonts-cantarell fonts-fantasque-sans libqt5svg5 qml-module-qtquick-controls openssh-client openssh-server ttf-mscorefonts-installer
-
-# download Nordic Theme
-git clone https://github.com/EliverLara/Nordic.git
-sudo cp -r Nordic /usr/share/themes/
-
-# copy input device configurations
-sudo cp additional-config-files/etc_X11_xorg.conf.d/* /etc/X11/xorg.conf.d/
-#sudo cp additional-config-files/usr_share_X11_xorg.conf.d/* /usr/share/X11/xorg.conf.d/
-
-# make theme folders
-mkdir -p ~/.themes ~/.fonts ~/.config
-
-# Copy user configuration files
-cp -r config-files/* ~/.config/
-
-# download kitty themes
-mkdir -p ~/.config/kitty/kitty-themes
-git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
-
-# data partition mount service (assuming using systemd)
-if [ $USER == mrizaln ]; then
-    sudo cp others/mnt-46B691EEB691DF2D.mount /etc/systemd/system/
-    sudo systemctl enable mnt-46B691EEB691DF2D.mount
+SYSTEM=$(cat /etc/os-release | grep -E ^ID | cut -d= -f2)
+echo "You are using: $SYSTEM"
+if [[ "$SYSTEM" != "fedora" || "$SYSTEM" != "debian" || "$SYSTEM" != "ubuntu" ]]; then
+    echo "I'm sorry but $SYSTEM is not configured for now"
+    echo "You need to install the packages manually"
+    echo "The config-files/ and scripts/ can still be used"
+    echo "You need to copy those manually as well"
+    exit 1
 fi
 
-# low battery warn service
-mkdir -p ~/.config/systemd/{user,scripts}
-cp additional-scripts/battery-warn.* ~/.config/systemd/user/
-cp additional-scripts/battery.sh ~/.config/systemd/scripts/
+update_system()
+{
+    if [[ "$SYSTEM" == "debian" || "$SYSTEM" == "ubuntu" ]]; then
+        sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+    elif [[ "$SYSTEM" == "fedora" ]]; then
+        sudo dnf update
+    else
+        echo "Unknown system"
+        return
+    fi
+}
 
-# download mpris indicator for polybar
-#wget https://raw.githubusercontent.com/polybar/polybar-scripts/master/polybar-scripts/player-mpris-tail/player-mpris-tail.py
-#wget https://raw.githubusercontent.com/polybar/polybar-scripts/master/polybar-scripts/pulseaudio-microphone/pulseaudio-microphone.sh
-#chmod +x player-mpris-tail.py pulseaudio-microphone.sh
-#mv player-mpris-tail.py pulseaudio-microphone.sh ~/.config/polybar/polybar-scripts/
+install_package()
+{
+    local cmd
+    if [[ "$SYSTEM" == fedora ]]; then
+        cmd="sudo dnf install -y"
+    elif [[ "$SYSTEM" == "debian" || "$SYSTEM" == "ubuntu" ]]; then
+        cmd="sudo apt install -y"
+    else
+        echo "Unknown system"
+        return
+    fi
+    $cmd "$@"
+}
 
-# install Nerd Font
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
-unzip JetBrainsMono.zip -d ~/.fonts/
-fc-cache -vf
+install_essential()
+{
+    install_package bspwm polybar rofi picom sxhkd dunst git unzip rxvt-unicode thunar alsa-utils pavucontrol feh mpv vnstat net-tools brightnessctl sddm
+}
 
-# X appearance
-sed "s/mrizaln/$USER/" -i .Xresources       # if username is not mrizaln, replace it with $USER
-cp .Xresources ~
-cp .Xnord ~
+install_nonessential()
+{
+    local ssh_client=$([[ "$SYSTEM" == "fedora" ]] && echo openssh-clients || echo openssh-client)
+    install_package htop neofetch lxpolkit lxappearance papirus-icon-theme openssh-server $ssh_client
+}
 
-# installing rtl8821ce driver, just in case
-if [[ "$(lspci | grep -i rtl8821ce | wc -l)" == 1 ]]; then
-    sudo apt install bc module-assistant build-essential dkms
-    git clone https://github.com/tomaspinho/rtl8821ce
-    cd rtl8821ce
-    sudo ./dkms-install.sh
-fi
+install_appearance()
+{
+    # make theme folders
+    mkdir -p ~/.themes ~/.fonts
+
+    # fonts
+    if [[ "$SYSTEM" == fedora ]]; then
+        sudo dnf install -y google-noto-cjk-fonts google-noto-color-emoji-fonts fira-code-fonts jetbrains-mono-fonts fontawesome-fonts
+
+        # ubuntu fonts
+        sudo dnf copr enable atim/ubuntu-fonts -y && sudo dnf install -y ubuntu-family-fonts
+
+        # microsoft fonts
+        sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+        sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+    elif [[ "$SYSTEM" == "debian" || "$SYSTEM" == "ubuntu" ]]; then
+        sudo apt install -y fonts-noto-cjk fonts-noto-color-emoji fonts-firacode fonts-jetbrains-mono fonts-font-awesome fonts-ubuntu fonts-cantarell fonts-fantasque-sans ttf-mscorefonts-installer
+    fi
+
+    # nerd fonts
+    #
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+    unzip FiraCode.zip -d ~/.fonts
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+    unzip Meslo.zip -d ~/.fonts
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+    unzip JetBrainsMono.zip -d ~/.fonts/
+    fc-cache -vf
+ 
+    # download Nordic Theme
+    git clone https://github.com/EliverLara/Nordic.git
+    sudo cp -r Nordic /usr/share/themes/
+}
+
+configure_system()
+{
+    mkdir -p ~/.config
+ 
+    # copy user configuration files
+    cp -r config-files/* ~/.config/
+
+    # copy scripts
+    mkdir -p ~/.local/bin
+    cp -r scripts/* ~/.local/bin
+
+    # low battery warn service
+    mkdir -p ~/.config/systemd/{user,scripts}
+    cp additional-scripts/battery-warn.* ~/.config/systemd/user/
+    cp additional-scripts/battery.sh ~/.config/systemd/scripts/
+
+    # X appearance
+    sed "s/mrizaln/$USER/" -i .Xresources       # if username is not mrizaln, replace it with $USER
+    cp .Xresources ~
+    cp .Xnord ~
+
+    # copy input device configurations
+    sudo cp additional-config-files/etc_X11_xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/
+}
+
+update_system
+install_essential
+install_nonessential
+install_appearance
+configure_system
 
 # installing network-manager package
 # it needs to be the last, because for some reason it makes already connected network unreachable
-sudo apt install network-manager
+install_package network-manager$([[ "$SYSTEM" == "fedora" ]] && echo "-applet")
 
 echo -e "\n\n\n==============[ NOW RESTART YOUR COMPUTER ]==============="
